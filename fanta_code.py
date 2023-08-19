83f634db4677b8c5e19c4af17bdfc2dfc6a89fac
aruta <matteoaruta6@gmail.com>
2023-08-20 00:57:13 +0200

    modificato codice con 2 dataset e aggiunto il sito online

--- fanta_code.py
+++ fanta_code.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 # Carica il dataset da un file CSV
-dataset = pd.read_csv("C:\\Users\\aruta\\OneDrive\\Desktop\\fanta\\seriea.csv", sep=",")
+dataset = pd.read_csv("C:\\Users\\aruta\\OneDrive\\Desktop\\app_fanta\\seriea2022_23.csv", sep=",")
+dataset2 = pd.read_csv("C:\\Users\\aruta\\OneDrive\\Desktop\\app_fanta\\seriea2023_24.csv", sep=",")
+
 
 # Richiedi le soglie all'utente
 soglia_voto_minimo = float(input("Inserisci la soglia di voto minima: "))
@@ -16,46 +18,50 @@ soglia_esp_max = float(input("Inserisci la soglia di espulsioni massime: "))
 soglia_au_max = float(input("Inserisci la soglia di autogol massimi: "))
 
 # Filtra i giocatori in base ai criteri specificati per ciascun ruolo
-portieri_selezionati = dataset[
-    (dataset['Mv'] >= soglia_voto_minimo) & (dataset['Mv'] < soglia_voto_massimo) & 
-    (dataset['Pv'] > 10) & (dataset['R'] == 'P') & 
-    (dataset['Rp'] >= soglia_rp_minimi_parati) & 
-    (dataset['Amm'] <= soglia_amm_max) & (dataset['Esp'] <= soglia_esp_max) & 
-    (dataset['Au'] <= soglia_au_max)
-].sort_values(by='Mf', ascending=False)
+lunghezza = max(dataset.shape[0], dataset2.shape[0])
 
-difensori_selezionati = dataset[
-    (dataset['Mf'] >= soglia_voto_minimo) & (dataset['Mf'] < soglia_voto_massimo) & 
-    (dataset['R'] == 'D') & 
-    (dataset['Rc'] >= soglia_rc_minimi_calciati) & (dataset['R-'] <= soglia_r_errati_max) &
-    (dataset['Amm'] <= soglia_amm_max) & (dataset['Esp'] <= soglia_esp_max) & 
-    (dataset['Au'] <= soglia_au_max)
-].sort_values(by='Mf', ascending=False)
+for i in range(0,1):
+    if dataset["Nome"][i] in dataset2["Nome"].values:
+        portieri_selezionati = dataset[
+        (dataset['Mv'] >= soglia_voto_minimo) & (dataset['Mv'] < soglia_voto_massimo) & 
+        (dataset['Pv'] > 10) & (dataset['R'] == 'P') & 
+        (dataset['Rp'] >= soglia_rp_minimi_parati) & 
+        (dataset['Amm'] <= soglia_amm_max) & (dataset['Esp'] <= soglia_esp_max) & 
+        (dataset['Au'] <= soglia_au_max)
+    ].sort_values(by='Fm', ascending=False)
 
-centrocampisti_selezionati = dataset[
-    (dataset['Mf'] >= soglia_voto_minimo) & (dataset['Mf'] < soglia_voto_massimo) & 
-    (dataset['R'] == 'C') & 
-    (dataset['Rc'] >= soglia_rc_minimi_calciati) & (dataset['R-'] <= soglia_r_errati_max) &
-    (dataset['Amm'] <= soglia_amm_max) & (dataset['Esp'] <= soglia_esp_max) & 
-    (dataset['Au'] <= soglia_au_max)
-].sort_values(by='Mf', ascending=False)
+    difensori_selezionati = dataset[
+        (dataset['Fm'] >= soglia_voto_minimo) & (dataset['Fm'] < soglia_voto_massimo) & 
+        (dataset['R'] == 'D') & 
+        (dataset['Rc'] >= soglia_rc_minimi_calciati) & (dataset['R-'] <= soglia_r_errati_max) &
+        (dataset['Amm'] <= soglia_amm_max) & (dataset['Esp'] <= soglia_esp_max) & 
+        (dataset['Au'] <= soglia_au_max)
+    ].sort_values(by='Fm', ascending=False)
 
-attaccanti_selezionati = dataset[
-    (dataset['Mf'] >= soglia_voto_minimo) & (dataset['Mf'] < soglia_voto_massimo) & 
-    (dataset['R'] == 'A') & 
-    (dataset['Rc'] >= soglia_rc_minimi_calciati) & (dataset['R-'] <= soglia_r_errati_max) &
-    (dataset['Amm'] <= soglia_amm_max) & (dataset['Esp'] <= soglia_esp_max) & 
-    (dataset['Au'] <= soglia_au_max)
-].sort_values(by='Mf', ascending=False)
+    centrocampisti_selezionati = dataset[
+        (dataset['Fm'] >= soglia_voto_minimo) & (dataset['Fm'] < soglia_voto_massimo) & 
+        (dataset['R'] == 'C') & 
+        (dataset['Rc'] >= soglia_rc_minimi_calciati) & (dataset['R-'] <= soglia_r_errati_max) &
+        (dataset['Amm'] <= soglia_amm_max) & (dataset['Esp'] <= soglia_esp_max) & 
+        (dataset['Au'] <= soglia_au_max)
+    ].sort_values(by='Fm', ascending=False)
 
-# Seleziona i migliori giocatori per ciascun ruolo
-portieri_finali = portieri_selezionati.head(3)
-difensori_finali = difensori_selezionati.head(8)
-centrocampisti_finali = centrocampisti_selezionati.head(8)
-attaccanti_finali = attaccanti_selezionati.head(6)
+    attaccanti_selezionati = dataset[
+        (dataset['Fm'] >= soglia_voto_minimo) & (dataset['Fm'] < soglia_voto_massimo) & 
+        (dataset['R'] == 'A') & 
+        (dataset['Rc'] >= soglia_rc_minimi_calciati) & (dataset['R-'] <= soglia_r_errati_max) &
+        (dataset['Amm'] <= soglia_amm_max) & (dataset['Esp'] <= soglia_esp_max) & 
+        (dataset['Au'] <= soglia_au_max)
+    ].sort_values(by='Fm', ascending=False)
 
-# Combina i giocatori di tutti i ruoli per formare la squadra completa
-squadra_fantacalcio = pd.concat([portieri_finali, difensori_finali, centrocampisti_finali, attaccanti_finali])
+    # Seleziona i migliori giocatori per ciascun ruolo
+    portieri_finali = portieri_selezionati.head(3)
+    difensori_finali = difensori_selezionati.head(8)
+    centrocampisti_finali = centrocampisti_selezionati.head(8)
+    attaccanti_finali = attaccanti_selezionati.head(6)
 
-# Stampa la squadra di fantacalcio risultante
-print(squadra_fantacalcio)
+    # Combina i giocatori di tutti i ruoli per formare la squadra completa
+    squadra_fantacalcio = pd.concat([portieri_finali, difensori_finali, centrocampisti_finali, attaccanti_finali])
+
+    # Stampa la squadra di fantacalcio risultante
+    print(squadra_fantacalcio)

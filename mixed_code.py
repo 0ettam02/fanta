b9b19b64db75087e186143deecf45d175404ef65
aruta <matteoaruta6@gmail.com>
2023-08-25 11:54:42 +0200

    creata homepage con codice di creazione squadra(mixed_code_flask.py)

--- mixed_code.py
+++ mixed_code.py
@@ -1,7 +1,39 @@
 from flask import Flask, render_template, request
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+def load_datasets():
+    dataset1 = pd.read_csv("C:\\Users\\aruta\\OneDrive\\Desktop\\app_fanta\\flask\\seriea2022_23.csv", sep=",")
+    dataset2 = pd.read_csv("C:\\Users\\aruta\\OneDrive\\Desktop\\app_fanta\\flask\\seriea2023_24.csv", sep=",")
+    return dataset1, dataset2
+
+def train_model(dataset):
+    X = dataset.drop(['Gf', 'R', 'Nome', 'Squadra', 'Id', 'Rm'], axis=1).values
+    y = dataset['Gf'].values
+
+    X_train, _, y_train, _ = train_test_split(X, y, test_size=0.3)
+
+    ss = StandardScaler()
+    X_train = ss.fit_transform(X_train)
+
+    model = Sequential()
+    model.add(Dense(12, input_dim=X_train.shape[1], activation='relu'))
+    model.add(Dense(1, activation='linear'))
+
+    model.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
+
+    model.fit(X_train, y_train, epochs=100)
+
+    return model, ss
+
+dataset1, dataset2 = load_datasets()
+model, ss = train_model(dataset1)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -69,6 +101,25 @@ def index():
         return render_template('results.html', squadra=squadra_fantacalcio)
 
     return render_template('squadra.html')
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if request.method == "POST":
+        giocatore = request.form["giocatore"]
+
+        if giocatore in dataset2['Nome'].values:
+            giocatore_data = dataset1[dataset1['Nome'] == giocatore].drop(['Gf', 'R', 'Nome', 'Squadra', 'Id', 'Rm'], axis=1).values
+
+            giocatore_data_normalized = ss.transform(giocatore_data)
+
+            predicted = model.predict(giocatore_data_normalized)
+
+            prediction = predicted[0][0]
+            return render_template("results_pred_squad.html", giocatore=giocatore, prediction=prediction)
+        else:
+            return render_template("results_pred_squad.html", giocatore=giocatore, prediction=None)
+
+    return render_template("pred_squad.html")
 
 if __name__ == '__main__':
     app.run(debug=True)

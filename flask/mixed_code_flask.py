42628ad060fc1ca6cd5c7c03c07fdfb595b657f9
aruta <matteoaruta6@gmail.com>
2023-08-26 13:00:11 +0200

    creata homepage funzionante con creazione squadra e predizione statistiche e modificata parte estetica

--- flask/mixed_code_flask.py
+++ flask/mixed_code_flask.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense
 
 app = Flask(__name__)
 
+
+#RETURN AI CODICI-----------------------------------------------------------------------------
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
+    action = request.form.get("action")
+    if request.method == 'POST' and action == 'squadra':
         return redirect(url_for('squadra'))
+    elif request.method == 'POST' and action == 'predict':
+        return redirect(url_for('predict'))
     return render_template('index.html')
 
+
+#CODICE DATA MINING CREZZIONE SQUADRA----------------------------------------------------------
 @app.route('/squadra', methods=['GET', 'POST'])
 def squadra():
     global squadra_fantacalcio
@@ -71,10 +84,65 @@ def squadra():
         return redirect(url_for('results'))
     return render_template('squadra.html')
 
+
+#CODICE RETE NEURAKE ARTIFICIALE PREVISIONE STATS-------------------------------------
+@app.route('/pred_stats', methods=['GET', 'POST'])
+def train_model():
+    dataset1 = pd.read_csv("C:\\Users\\aruta\\OneDrive\\Desktop\\app_fanta\\flask\\seriea2022_23.csv", sep=",")
+
+    X = dataset1.drop(['Gf', 'R', 'Nome', 'Squadra', 'Id', 'Rm'], axis=1).values
+    y = dataset1['Gf'].values
+
+    X_train, _, y_train, _ = train_test_split(X, y, test_size=0.3)
+
+    ss = StandardScaler()
+    X_train = ss.fit_transform(X_train)
+
+    model = Sequential()
+    model.add(Dense(12, input_dim=X_train.shape[1], activation='relu'))
+    model.add(Dense(1, activation='linear'))
+
+    model.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
+
+    model.fit(X_train, y_train, epochs=100)
+
+    return model, ss
+
+model, ss = train_model()
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    global giocatore, prediction
+    dataset1 = pd.read_csv("C:\\Users\\aruta\\OneDrive\\Desktop\\app_fanta\\flask\\seriea2022_23.csv", sep=",")
+    dataset2 = pd.read_csv("C:\\Users\\aruta\\OneDrive\\Desktop\\app_fanta\\flask\\seriea2023_24.csv", sep=",")
+
+    if request.method == "POST":
+        giocatore = request.form["giocatore"]
+
+        if giocatore in dataset2['Nome'].values:
+            giocatore_data = dataset1[dataset1['Nome'] == giocatore].drop(['Gf', 'R', 'Nome', 'Squadra', 'Id', 'Rm'], axis=1).values
+
+            giocatore_data_normalized = ss.transform(giocatore_data)
+
+            predicted = model.predict(giocatore_data_normalized)
+
+            prediction = predicted[0][0]
+            return redirect(url_for('results_stats'))
+
+    return render_template("pred_squad.html")
+
+
+#VISUALIZZAZIONE RISULTATI CREAZIONE SQUADRA----------------------------------------------
 @app.route('/results')
 def results():
-    return render_template('results.html', squadra=squadra_fantacalcio)
+        return render_template('results.html', squadra=squadra_fantacalcio)
+    
+#VISUALIZZAZIONE RISULTATI PREVISIONE STATISTICHE------------------------------------------
+@app.route('/results_stats')
+def results_stats():
+    return render_template("results_pred_squad.html", giocatore=giocatore, prediction=prediction)
+    
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
-
